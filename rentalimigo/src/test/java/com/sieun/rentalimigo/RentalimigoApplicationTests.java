9514780b1acb68fec4e5a8cb16ef82f8f2ffbb90
SIEUN KIM <sieun043@gmail.com>
2025-08-25 14:07:13 +0900

    create(backend): create back-end project file

---
+++ rentalimigo/src/test/java/com/sieun/rentalimigo/RentalimigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sieun.rentalimigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalimigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
