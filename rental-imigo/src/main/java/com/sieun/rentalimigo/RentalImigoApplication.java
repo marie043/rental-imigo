a590e2e98d092b0c18426ee9d1bac3aac0b353d1
SIEUN KIM <sieun043@gmail.com>
2025-08-31 15:54:58 +0900

    create(backend): create backe-end project file

---
+++ rental-imigo/src/main/java/com/sieun/rentalimigo/RentalImigoApplication.java
@@ -0,0 +1,13 @@
+package com.sieun.rentalimigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalImigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalImigoApplication.class, args);
+	}
+
+}
